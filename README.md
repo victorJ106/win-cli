c977f334eee2885b2312beb939944a17fe75450d
victorJ106 <victorj818@163.com>
2021-01-24 15:29:14 +0800

    feature: 新增脚手架命令init <projectName>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # win-cli
 A Tooling for Web Front-End Development
+
+# 脚手架命令
+```shell
+win-cli init <projectName>  // 初始化项目 
+```
---
+++ bin/win.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+program.version(require('../package.json').version)
+program
+  .command('init <name>')
+  .description('init a project')
+  .action(require('../lib/init'))
+  
+program.parse(process.args)
